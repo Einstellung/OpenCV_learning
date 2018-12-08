c2e63e8e6d3000f75458880b52e96dccf4c55e20
Einstellung <kekedog@126.com>
2018-12-08 22:13:42 +0800

    OpenCV seventh merge

--- OpenCV/source/conf.py
+++ OpenCV/source/conf.py
@@ -115,35 +115,6 @@ html_static_path = ['_static']
 htmlhelp_basename = 'OpenCVdoc'
 
 
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'OpenCV.tex', u'OpenCV Documentation',
-     u'Hunag Xinyuan', 'manual'),
-]
-
-
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
