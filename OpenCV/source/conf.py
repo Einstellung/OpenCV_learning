92ddeb2dab77664f11ed130045232a6963bbd915
Einstellung <kekedog@126.com>
2018-12-09 10:57:08 +0800

    OpenCV ninth merge

--- OpenCV/source/conf.py
+++ OpenCV/source/conf.py
@@ -159,3 +159,23 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Extension configuration -------------------------------------------------
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements={# The paper size ('letterpaper' or 'a4paper').
+'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+'inputenc':'',#必須
+'utf8extra':'',#必須
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""}
